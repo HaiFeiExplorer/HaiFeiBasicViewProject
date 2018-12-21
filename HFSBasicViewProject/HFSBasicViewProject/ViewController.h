e063637ea57e8327d30d5fe98e35c681311ba5e3
panlisha <panlisha163@163.com>
2018-12-21 14:48:30 +0800

    init project

---
+++ HFSBasicViewProject/HFSBasicViewProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HFSBasicViewProject
+//
+//  Created by unionx on 2018/12/21.
+//  Copyright © 2018年 HFS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HFSBasicViewProject/HFSBasicViewProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HFSBasicViewProject
+//
+//  Created by unionx on 2018/12/21.
+//  Copyright © 2018年 HFS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
